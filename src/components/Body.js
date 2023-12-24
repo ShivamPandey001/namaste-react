21b7451303152722056da0e34c9ef187cb517060
Shivam Pandey <54200130+ShivamPandey001@users.noreply.github.com>
2023-12-24 11:32:40 +0530

    Exploring the world, added searchFunctionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,22 @@
 import RestuarantCard from "./RestuarantCard";
-import resList from "../utils/mockData";
 import { useState, useEffect } from "react";
+import ShimmerUi from "./ShimmerUi";
 
 const Body = () => {
-    const [listOfRes , setListOfRestuarant] = useState(resList);
+    const [listOfRes , setListOfRestuarant] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
     useEffect(()=>{
-        console.log("useEffect called");
+        fetchData();
     }, []);
+
+    const fetchData = async() => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.342956&lng=81.909677&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        setListOfRestuarant(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+    
     console.log("renderrring body");
     /**
      * it can also be done the below way:
@@ -15,14 +24,35 @@ const Body = () => {
      * - const listOfRes = arr[0];
      * - const setListOfRestuarant = arr[1];
      */
-    return (
+     return !listOfRes || listOfRes.length ===0 ? <ShimmerUi /> : (
       <div>
         <div className="filter">
+            <div className="search">
+                <input className="search-box" type="text" value = {searchText}
+                    onChange={(e) =>{
+                        setSearchText(e.target.value);
+                    }}
+                />
+                <button className="searchBtn"
+                    onClick={()=>
+                    {
+                        console.log(searchText);
+                        
+                        const filteredRestuarant = listOfRes.filter((res) => (
+                           res.info.name.toLowerCase().includes(searchText.toLowerCase())));
+                        console.log(listOfRes);
+                        console.log("data " +filteredRestuarant);
+                        setListOfRestuarant(filteredRestuarant);
+                        console.log(filteredRestuarant);
+                    }
+                    }
+                >Search</button>
+            </div>
           <button
             className="filter-btn"
             onClick={() => {
               const filteredRes = listOfRes.filter(
-                (res) => res.info.avgRating > 4.5
+                (res) => res.info.avgRating > 4
               );
               console.log(filteredRes);
               setListOfRestuarant(filteredRes);
